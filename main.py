c2bc04e3eeee593d95fdb14136c6412e13ca7800
onetuks <onetuks@naver.com>
2024-12-31 02:17:55 +0900

    Chore: 프로젝트 설정 추가

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.common.config import conf
+from app.routes import index
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def init_app():
+  instance = FastAPI()
+
+  # 환경 정의
+  # config = conf()
+  # conf_dict = asdict(config)
+
+  # 미들웨어 정의
+
+  # 라우터 정의
+  instance.include_router(index.router)
+
+  return instance
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app = init_app()
+
+if __name__ == "__main__":
+  uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=conf().PROJ_RELOAD)
