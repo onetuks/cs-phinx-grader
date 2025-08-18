06ba5af68bb46f9aa60ade04bf46cfe546264552
onetuks <onetuks@naver.com>
2025-08-18 21:51:29 +0900

    Refactor: Camel Case 대응토록 보완

--- src/app/schemas/grade_request.py
+++ src/app/schemas/grade_request.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel, Field
 
 
 class GradeRequest(BaseModel):
-    user_answer: str = Field(..., description="제출답안")
-    desirable_answers: List[str] = Field(..., description="모범답안")
+    user_answer: str = Field(..., description="제출답안", alias="userAnswer")
+    desirable_answers: List[str] = Field(..., description="모범답안", alias="desirableAnswers")
